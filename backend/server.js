140c9b9033e1a3e73cf6f32f68d58530b05dbff7
Arya <jjarya2003@gmail.com>
2026-06-01 16:31:55 +0530

    Mysql db setup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "API Running Successfully"
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
